1d300162e17321a2aed1a6e9aa74e619a6d49a50
scholastica25 <schogbonnamara@gmail.com>
2024-01-07 16:06:33 -0800

    0-hbtn_status.py

--- 0x11-python-network_1/0-hbtn_status.py
+++ 0x11-python-network_1/0-hbtn_status.py
@@ -1,15 +1,12 @@
 #!/usr/bin/python3
-"""fetches https://alx-intranet.hbtn.io/status"""
-import urllib.request
+"""Python script that fetches https://alx-intranet.hbtn.io/status"""
 
+from urllib import request
 
 if __name__ == "__main__":
-    """fetches https://alx-intranet.hbtn.io/status"""
-    with urllib.request.urlopen('https://alx-intranet.hbtn.io/status') as response:
-        html = response.read()
-        html_str = html.decode('utf-8')
-
-    print("Body response:")
-    print("\t- type: {}".format(type(html)))
-    print("\t- content: {}".format(html))
-    print("\t- utf8 content: {}".format(html_str))
+    with request.urlopen("https://alx-intranet.hbtn.io/status") as page:
+        content = page.read()
+        print("Body response:")
+        print("\t- type: {}".format(type(content)))
+        print("\t- content: {}".format(content))
+        print("\t- utf8 content: {}".format(content.decode('utf-8')))
