574b3bb4516e132afa0838aee8776c37888f75ab
scholastica25 <schogbonnamara@gmail.com>
2024-01-07 16:27:02 -0800

    updated

--- 0x11-python-network_1/100-github_commits.py
+++ 0x11-python-network_1/100-github_commits.py
@@ -1,25 +1,16 @@
 #!/usr/bin/python3
-"""
-list 10 commits (from the most recent to oldest) of the repository “rails”
-by the user “rails”
-"""
+"""Python script that lists 10 commits (from the most recent
+to oldest) of the repository “rails” by the user “rails”."""
+
 import requests
 from sys import argv
 
-
 if __name__ == "__main__":
-    """
-    list 10 commits (from the most recent to oldest) of the repository
-    “rails” by the user “rails”
-    """
-    repo = argv[1]
-    owner = argv[2]
-    url = 'https://api.github.com/repos/{}/{}/commits'.format(owner, repo)
-    r = requests.get(url)
-    res_list = r.json()
-    try:
-        for i in range(10):
-            print("{}: {}".format(res_list[i].get('sha'), res_list[i].
-                                  get('commit').get('author').get('name')))
-    except:
-        pass
+    url = "https://api.github.com/repos/{}/{}/commits?per_page=10"\
+          .format(argv[2], argv[1])
+
+    request = requests.get(url)
+    commits = request.json()
+    for commit in commits:
+        print("{}: {}".format(commit.get("sha"),
+                              commit.get("commit").get("author").get("name")))
